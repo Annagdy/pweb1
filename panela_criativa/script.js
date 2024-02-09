410b64a66d9d21b126182ed77d510e86713cde4e
amadoiuri <105980873+amadoiuri@users.noreply.github.com>
2024-02-09 10:22:29 -0300

    Update script.js

--- panela_criativa/script.js
+++ panela_criativa/script.js
@@ -18,3 +18,123 @@ document.addEventListener("DOMContentLoaded", function() {
         carousel.style.transform = 'translateX(' + translateValue + ')';
     }
 });
+
+
+
+function signInWithGoogle() {
+    const authUrl = 'https://accounts.google.com/o/oauth2/auth';
+    const clientId = 'SEU_ID_DE_CLIENTE';
+    const redirectUri = 'URL_DE_RETORNO';
+    const scope = 'https://www.googleapis.com/auth/userinfo.profile';
+    const authLink = `${authUrl}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code`;
+  
+    window.location.href = authLink;
+  }
+  
+  // Adicionando um ouvinte de evento ao link
+  document.getElementById("googleSignInLink").addEventListener("click", function() {
+    signInWithGoogle();
+  });
+  
+  function addStep() {
+    const stepsList = document.getElementById('stepsList');
+    const newStep = document.createElement('li');
+    newStep.innerHTML = '<label>Passo ' + (stepsList.childElementCount + 1) + ':</label>' +
+                        '<textarea name="step' + (stepsList.childElementCount + 1) + '" required></textarea>' +
+                        '<label>Imagem do Passo:</label>' +
+                        '<input type="file" name="stepImage' + (stepsList.childElementCount + 1) + '" accept="image/*">';
+    stepsList.appendChild(newStep);
+}
+
+function removeStep() {
+    const stepsList = document.getElementById('stepsList');
+    if (stepsList.childElementCount > 0) {
+        stepsList.removeChild(stepsList.lastChild);
+    }
+}
+
+function createCheckboxes() {
+  var ingredientsTextArea = document.getElementById('ingredients');
+  var ingredientsListDiv = document.getElementById('ingredientsList');
+  
+  // Limpa a lista de checkboxes existente
+  ingredientsListDiv.innerHTML = '';
+
+  // Separa os ingredientes por linha
+  var ingredientsArray = ingredientsTextArea.value.split('\n');
+
+  // Cria uma checkbox para cada ingrediente
+  ingredientsArray.forEach(function(ingredient) {
+      var checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.name = 'ingredientCheckbox';
+      checkbox.value = ingredient.trim(); // Remove espaços em branco extras
+      var label = document.createElement('label');
+      label.appendChild(checkbox);
+      label.appendChild(document.createTextNode(ingredient.trim()));
+      
+      ingredientsListDiv.appendChild(label);
+  });
+}
+
+// Adiciona um ouvinte de evento para chamar a função quando o conteúdo do textarea muda
+document.getElementById('ingredients').addEventListener('input', createCheckboxes);
+
+// Chama a função inicialmente para criar as checkboxes se houver algum conteúdo preexistente no textarea
+createCheckboxes();
+
+
+
+
+
+// Função para formatar texto (negrito, itálico)
+function formatText(command) {
+  document.execCommand(command, false, null);
+}
+
+// Função para inserir link
+function insertLink() {
+  var url = prompt("Insira o URL do link:");
+  if (url) {
+      document.execCommand("createLink", false, url);
+  }
+}
+
+// Função para inserir imagem
+function insertImage() {
+  var url = prompt("Insira o URL da imagem:");
+  if (url) {
+      document.execCommand("insertImage", false, url);
+  }
+}
+
+
+document.getElementById('searchInput').addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    search();
+  }
+});
+
+function search() {
+  // Implemente a lógica de pesquisa aqui
+  var searchTerm = document.getElementById('searchInput').value;
+  // Faça algo com o termo de pesquisa, por exemplo, redirecione para uma página de resultados
+  window.location.href = 'resultados.html?q=' + encodeURIComponent(searchTerm);
+}
+
+
+
+function validateLogin() {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+
+  // Verificação de login e senha padrão (exemplo)
+  if (email === 'seuemail@example.com' && password === 'suasenha') {
+    alert('Login bem-sucedido!');
+    window.location.href = 'perfil.html'; // Redireciona para a página de perfil
+    return false; // Evita o envio do formulário
+  } else {
+    alert('Login ou senha incorretos.');
+    return false; // Evita o envio do formulário
+  }
+}
