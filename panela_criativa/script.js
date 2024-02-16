ed4df315da75ae75602be0b95aa18f569d307c1b
Annagdy <annabeatriz_2019@outlook.com>
2024-02-16 09:02:48 -0300

    resul, pagina inicial

--- panela_criativa/script.js
+++ panela_criativa/script.js
@@ -138,3 +138,20 @@ function validateLogin() {
     return false; // Evita o envio do formulário
   }
 }
+
+function collapse(){
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("type");
+      var content = this.getElementsByClassName("type")
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+}
